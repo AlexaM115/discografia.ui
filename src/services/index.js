069587173df4d187cfe1d045a6fe141472f4b5f9
AlexaM115 <amymartinez2005mr@gmail.com>
2025-08-14 22:13:13 -0600

    Updates

--- src/services/index.js
+++
@@ -1,4 +0,0 @@
-export { authService } from './authService.js'
-export { artistTypeService } from './artistTypeService.js'
-export { artistService } from './artistService.js'
-export { API_BASE_URL } from './api.js'
